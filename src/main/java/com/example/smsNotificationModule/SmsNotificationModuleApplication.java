031882a3a2df17aa2e8118f6733f3b07107a5ffc
daviemukungi <mukungidavid@gmail.com>
2021-11-09 21:40:09 +0300

    first commit

---
+++ src/main/java/com/example/smsNotificationModule/SmsNotificationModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.smsNotificationModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsNotificationModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsNotificationModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smsNotificationModule/SmsNotificationModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smsNotificationModule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsNotificationModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
